c61e0600670043a25e09cc1992a7c4d678f1d65f
Barkar <lemongrass@yandex.ru>
2024-01-24 17:55:26 +0100

    first test UECS

---
+++ Assets/Code/_Temp/SpawnerSystem.cs
@@ -0,0 +1,52 @@
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public partial struct SpawnerSystem : ISystem
+{
+    public void OnCreate (ref SystemState state){}
+    
+    public void OnDestroy(ref SystemState state){}
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        EntityCommandBuffer.ParallelWriter ecb = GetEntityCommandBuffer(ref state);
+
+        new ProcessSpawnerJob()
+        {
+            ElapsedTime = SystemAPI.Time.ElapsedTime,
+            Ecb = ecb
+        }.ScheduleParallel();
+    }
+
+    private EntityCommandBuffer.ParallelWriter GetEntityCommandBuffer(ref SystemState state)
+    {
+        var ecbSingleton = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        var ecb = ecbSingleton.CreateCommandBuffer(state.WorldUnmanaged);
+        return ecb.AsParallelWriter();
+    }
+    
+
+    [BurstCompile]
+    public partial struct ProcessSpawnerJob : IJobEntity
+    {
+        public EntityCommandBuffer.ParallelWriter Ecb;
+        public double ElapsedTime;
+        
+        private void Execute([ChunkIndexInQuery] int chunkIndex, ref Spawner spawner)
+        {
+            if (spawner.NextSpawnTime < ElapsedTime)
+            {
+                spawner.SpawnPosition += new float3(1f, 1f, 0f);
+                Entity newBullet = Ecb.Instantiate(chunkIndex, spawner.Prefab);
+                Ecb.SetComponent(chunkIndex, newBullet, LocalTransform.FromPosition(spawner.SpawnPosition));
+                spawner.NextSpawnTime = (float)ElapsedTime + spawner.SpawnRate;
+                
+            }
+        }
+    }
+}
+
+
